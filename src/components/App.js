f958f63440e24141236ad4c924ba0e32c9fa176d
rohankulkarni19 <giveyourbest360@gmail.com>
2026-08-13 21:15:04 +0530

    button vanish

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import TodoList from "./TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a React app', completed: false },
+    { id: 3, text: 'Deploy the React app', completed: false }
+  ]);
+  const handleComplete =(id)=>{
+    setTodos((prevTodos)=>
+      prevTodos.map((todo)=>
+        todo.id===id?{...todo,completed:true}:todo
+  )
+      );
+  };
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete} />
     </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+function  TodoList ({todos ,handleComplete}){
+    return(
+        <div>
+            <h2>Child Component</h2>
+            <ul>
+                {todos.map((todo)=>(
+                    <li key ={todo.id}>
+                        <span>{todo.text}</span> 
+                        {!todo.completed &&(  
+                            <button onClick={()=>handleComplete(todo.id)}>Complete</button>
+                            
+                            )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+export default TodoList;
